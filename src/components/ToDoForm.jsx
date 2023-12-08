2e09e6442db5cc2a29fc7e77ee87938c72288d2d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-08 23:15:59 +0300

    changes

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -12,6 +12,7 @@ function ToDoForm() {
     return(
         <form onSubmit={handleSubmit}>
             <h1>TODO FORM</h1>
+            <h2></h2>
             <label htmlFor='todo' className="todo_label" name="todo">Todos</label>
             <input type="text" name="todo" className="todo_input" />
             <input type="submit" className="submit_btn" value="Submit"/>
