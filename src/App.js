94ae45eddc04210dc32d41f72649bbe241da706c
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-09 22:57:53 +0300

    light icon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import ToDoList from './components/ToDoList';
 import {todos} from '../src/todos';
 import { FaMoon } from "react-icons/fa";
+import { CiLight } from "react-icons/ci";
 
 function App() {
   return (
     <div className="App">
-      <FaMoon />
+      <div>
+        <FaMoon />
+        <CiLight />
+      </div>
+      
       <ToDoList todos={todos}/>
     </div>
   );
