a6812f8d913eef67a23cdbcb726c55e400953f96
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-09 22:56:09 +0300

    dark icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ToDoList from './components/ToDoList';
 import {todos} from '../src/todos';
+import { FaMoon } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
+      <FaMoon />
       <ToDoList todos={todos}/>
     </div>
   );
