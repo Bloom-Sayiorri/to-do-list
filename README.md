d7ac173950acf2b36dc66e8ef07db2ebb042cf92
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-07-07 22:16:32 +0300

    form styling complete

--- README.md
+++ README.md
@@ -68,3 +68,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+    background-color: rgba(2, 171, 255, 0.755);
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -20,56 +20,54 @@ const Form = () => {
         <div className={styles.wrapper}>
             <h2 className={styles.heading}>Add New Todo</h2>
             <form onSubmit ={handleSubmit} className={styles.form}>
-                <label className={styles.titleLabel}>
-                    Title: 
-                    <input
-                        type='text'
-                        name='title'
-                        placeholder='Enter todo...'
-                        value={formData.title}
-                        className={styles.title}
-                    />
-                </label>
-                <label className={styles.descriptionLabel}>
-                    Description: 
-                    <input
-                        type='text'
-                        name='description'
-                        placeholder='Enter description...'
-                        value={formData.description}
-                        className={styles.description}
-                    />
-                </label>
-                <label className={styles.statusLabel}>
-                    Status: 
-                    <input
-                        type='text'
-                        name='status'
-                        placeholder='Enter Todo...'
-                        value={formData.status}
-                        className={styles.status}
-                    />
-                </label>
-                <label className={styles.priorityLabel}> 
-                    Priority: 
-                    <input
-                        type='text'
-                        name='priority'
-                        placeholder='Enter Todo...'
-                        value={formData.priority}
-                        className={styles.priority}
-                    />
-                </label>
-                <label className={styles.dateCreatedLabel}>
-                    Date-Created: 
-                    <input
-                        type='text'
-                        name='dateCreated'
-                        placeholder='Enter Todo...'
-                        value={formData.dateCreated}
-                        className={styles.dateCreated}
-                    />
-                </label>
+                <label htmlFor='title' className={styles.titleLabel}>Title:</label>
+                <input
+                    type='text'
+                    name='title'
+                    id='title'
+                    placeholder='Enter todo...'
+                    value={formData.title}
+                    className={styles.title}
+                />
+                <label htmlFor='description' className={styles.descriptionLabel}>Description:</label>
+                <input
+                    type='text'
+                    name='description'
+                    id='description'
+                    placeholder='Enter description...'
+                    value={formData.description}
+                    className={styles.description}
+                />
+                <label htmlFor='status' className={styles.statusLabel}>Status:</label>
+                <input
+                    type='text'
+                    name='status'
+                    id='status'
+                    placeholder='Enter Todo...'
+                    value={formData.status}
+                    className={styles.status}
+                />
+                <label htmlFor='priority' className={styles.priorityLabel}>Priority:</label>
+                <input
+                    type='text'
+                    name='priority'
+                    id='priority'
+                    placeholder='Enter Todo...'
+                    value={formData.priority}
+                    className={styles.priority}
+                />
+                <label htmlFor='dateCreated' className={styles.dateCreatedLabel}>Date-Created:</label>
+                <input
+                    type='text'
+                    name='dateCreated'
+                    id='dateCreated'
+                    placeholder='Enter Todo...'
+                    value={formData.dateCreated}
+                    className={styles.dateCreated}
+                />
+                <button className={styles.submitBtn}>
+                    Submit
+                </button>
             </form>
         </div>
     </div>
